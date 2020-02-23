903d7a56f67944b00264fed8f0d7fd46372e7ac4
jwhowes <jacob.w.howes@durham.ac.uk>
2020-02-23 15:26:05 +0000

    Replaced spaces with tabs

--- src/main.js
+++ src/main.js
@@ -1,39 +1,39 @@
 var VSHADER_SOURCE =
-    "attribute vec4 a_Position;\n\
-    attribute vec4 a_Color;\n\
-    attribute vec4 a_Normal;\n\
-    uniform mat4 u_ModelMatrix;\n\
-    uniform mat4 u_NormalMatrix;\n\
-    uniform mat4 u_ViewMatrix;\n\
-    uniform mat4 u_ProjMatrix;\n\
-    varying vec4 v_Color;\n\
-    varying vec3 v_Position;\n\
-    varying vec3 v_Normal;\n\
-    void main(){\n\
-        gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;\n\
-        v_Position = vec3(u_ModelMatrix * a_Position);\n\
-        v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));\n\
-        v_Color = a_Color;\n\
-    }";
-
-var FSHADER_SOURCE =
-    "#ifdef GL_ES\n\
-    precision mediump float;\n\
-    #endif\n\
-    uniform vec3 u_LightColor;\n\
-	uniform vec3 u_LightPosition;\n\
-	uniform vec3 u_AmbientLight;\n\
-    varying vec4 v_Color;\n\
+	"attribute vec4 a_Position;\n\
+	attribute vec4 a_Color;\n\
+	attribute vec4 a_Normal;\n\
+	uniform mat4 u_ModelMatrix;\n\
+	uniform mat4 u_NormalMatrix;\n\
+	uniform mat4 u_ViewMatrix;\n\
+	uniform mat4 u_ProjMatrix;\n\
+	varying vec4 v_Color;\n\
 	varying vec3 v_Position;\n\
 	varying vec3 v_Normal;\n\
-    void main() {\n\
-        vec3 normal = normalize(v_Normal);\n\
+	void main(){\n\
+		gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;\n\
+		v_Position = vec3(u_ModelMatrix * a_Position);\n\
+		v_Normal = normalize(vec3(u_NormalMatrix * a_Normal));\n\
+		v_Color = a_Color;\n\
+	}";
+
+var FSHADER_SOURCE =
+	"#ifdef GL_ES\n\
+	precision mediump float;\n\
+	#endif\n\
+	uniform vec3 u_LightColor;\n\
+	uniform vec3 u_LightPosition;\n\
+	uniform vec3 u_AmbientLight;\n\
+	varying vec4 v_Color;\n\
+	varying vec3 v_Position;\n\
+	varying vec3 v_Normal;\n\
+	void main() {\n\
+		vec3 normal = normalize(v_Normal);\n\
 		vec3 lightDirection = normalize(u_LightPosition - v_Position);\n\
 		float nDotL = max(dot(lightDirection, normal), 0.0);\n\
 		vec3 diffuse = u_LightColor * v_Color.rgb * nDotL;\n\
 		vec3 ambient = 0.25 * u_AmbientLight * v_Color.rgb;\n\
-        gl_FragColor = vec4(diffuse + ambient, v_Color.a);\n\
-    }";
+		gl_FragColor = vec4(diffuse + ambient, v_Color.a);\n\
+	}";
 
 var modelMatrix = new Matrix4();
 var viewMatrix = new Matrix4();
@@ -43,126 +43,126 @@ var light_rotation = [-45];
 var light_r_i = 0;
 
 function wait(ms){
-    var d = new Date();
-    var d2 = null;
-    do { d2 = new Date(); }
-    while(d2-d < ms);
+	var d = new Date();
+	var d2 = null;
+	do { d2 = new Date(); }
+	while(d2-d < ms);
 }
 
 function main(){
-    var light_r_step = 2;
-    var i = 0;
-    while(light_rotation[i] < 45){
-        light_rotation.push(light_rotation[i] + light_r_step);
-        i += 1;
-    }
-    while(light_rotation[i] > -45){
-        light_rotation.push(light_rotation[i] - light_r_step);
-        i += 1;
-    }
-    var canvas = document.getElementById("webgl");
-    var gl = getWebGLContext(canvas);
-    if(!gl){
-        console.log("Failed to get rendering context.");
-        return;
-    }
-    if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
-        console.log("Failed to initialise shaders.");
-        return;
-    }
-    gl.clearColor(0, 0, 0, 1);
-    gl.enable(gl.DEPTH_TEST);
+	var light_r_step = 2;
+	var i = 0;
+	while(light_rotation[i] < 45){
+		light_rotation.push(light_rotation[i] + light_r_step);
+		i += 1;
+	}
+	while(light_rotation[i] > -45){
+		light_rotation.push(light_rotation[i] - light_r_step);
+		i += 1;
+	}
+	var canvas = document.getElementById("webgl");
+	var gl = getWebGLContext(canvas);
+	if(!gl){
+		console.log("Failed to get rendering context.");
+		return;
+	}
+	if(!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)){
+		console.log("Failed to initialise shaders.");
+		return;
+	}
+	gl.clearColor(0, 0, 0, 1);
+	gl.enable(gl.DEPTH_TEST);
 
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    var u_ModelMatrix = gl.getUniformLocation(gl.program, "u_ModelMatrix");
-    var u_NormalMatrix = gl.getUniformLocation(gl.program, "u_NormalMatrix");
-    var u_ViewMatrix = gl.getUniformLocation(gl.program, "u_ViewMatrix");
-    var u_ProjMatrix = gl.getUniformLocation(gl.program, "u_ProjMatrix");
-    var u_LightColor = gl.getUniformLocation(gl.program, "u_LightColor");
-    var u_LightPosition = gl.getUniformLocation(gl.program, "u_LightPosition");
-    var u_AmbientLight = gl.getUniformLocation(gl.program, "u_AmbientLight");
+	var u_ModelMatrix = gl.getUniformLocation(gl.program, "u_ModelMatrix");
+	var u_NormalMatrix = gl.getUniformLocation(gl.program, "u_NormalMatrix");
+	var u_ViewMatrix = gl.getUniformLocation(gl.program, "u_ViewMatrix");
+	var u_ProjMatrix = gl.getUniformLocation(gl.program, "u_ProjMatrix");
+	var u_LightColor = gl.getUniformLocation(gl.program, "u_LightColor");
+	var u_LightPosition = gl.getUniformLocation(gl.program, "u_LightPosition");
+	var u_AmbientLight = gl.getUniformLocation(gl.program, "u_AmbientLight");
 
-    // Set lighting info (white light coming from a bit to the right and above the camera (behind it too)):
-    gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
+	// Set lighting info (white light coming from a bit to the right and above the camera (behind it too)):
+	gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
 
 	var lightPos = new Vector3([0, 1, 0]);
 	lightPos.normalize();
-    gl.uniform3fv(u_LightPosition, lightPos.elements);
+	gl.uniform3fv(u_LightPosition, lightPos.elements);
 
-    var ambientLight = new Vector3([1, 1, 1]);
-    ambientLight.normalize();
-    gl.uniform3fv(u_AmbientLight, ambientLight.elements);
+	var ambientLight = new Vector3([1, 1, 1]);
+	ambientLight.normalize();
+	gl.uniform3fv(u_AmbientLight, ambientLight.elements);
 
-    viewMatrix.setLookAt(0, 5, 25, 0, 0, 0, 0, 1, 0);
-    projMatrix.setPerspective(30, canvas.width/canvas.clientHeight, 1, 100);
-    gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
-    gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
-    requestAnimationFrame(draw(gl, u_ModelMatrix, u_NormalMatrix))
+	viewMatrix.setLookAt(0, 5, 25, 0, 0, 0, 0, 1, 0);
+	projMatrix.setPerspective(30, canvas.width/canvas.clientHeight, 1, 100);
+	gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
+	gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
+	requestAnimationFrame(draw(gl, u_ModelMatrix, u_NormalMatrix))
 }
 
 function draw(gl, u_ModelMatrix, u_NormalMatrix){
-    return function(timestamp){
-        light_r_i = (light_r_i + 1) % light_rotation.length;
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        modelMatrix.setTranslate(0, 0, 0);
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(0, 6, -5);
-            modelMatrix.rotate(light_rotation[light_r_i], 0, 0, 1);
-            drawHangingLight(gl, u_ModelMatrix, u_NormalMatrix);
-        modelMatrix = popMatrix();
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(-5, 0, -10);
-            drawLamp(gl, u_ModelMatrix, u_NormalMatrix);
-            pushMatrix(modelMatrix);
-                modelMatrix.translate(0, -1, 0);
-                drawCoffeeTable(gl, u_ModelMatrix, u_NormalMatrix);
-            modelMatrix = popMatrix();
-        modelMatrix = popMatrix();
-        var n = initVertexBuffers(gl);
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(5, 0, -5);
-            modelMatrix.rotate(90, 0, 1, 0);
-            drawTableAndChairs(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelMatrix = popMatrix();
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(-5, 0, -15);
-            drawShelves(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelMatrix = popMatrix();
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(0, 1.5, -15);
-            drawTV(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelMatrix = popMatrix();
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(0, -2.5, 0)
-            drawGround(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelMatrix = popMatrix();
-        pushMatrix(modelMatrix);
-            modelMatrix.translate(0, 0, -20);
-            modelMatrix.rotate(90, 1, 0, 0);
-            drawGround(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelmatrix = popMatrix();
-        requestAnimationFrame(draw(gl, u_ModelMatrix, u_NormalMatrix));
-    }
+	return function(timestamp){
+		light_r_i = (light_r_i + 1) % light_rotation.length;
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		modelMatrix.setTranslate(0, 0, 0);
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(0, 6, -5);
+			modelMatrix.rotate(light_rotation[light_r_i], 0, 0, 1);
+			drawHangingLight(gl, u_ModelMatrix, u_NormalMatrix);
+		modelMatrix = popMatrix();
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(-5, 0, -10);
+			drawLamp(gl, u_ModelMatrix, u_NormalMatrix);
+			pushMatrix(modelMatrix);
+				modelMatrix.translate(0, -1, 0);
+				drawCoffeeTable(gl, u_ModelMatrix, u_NormalMatrix);
+			modelMatrix = popMatrix();
+		modelMatrix = popMatrix();
+		var n = initVertexBuffers(gl);
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(5, 0, -5);
+			modelMatrix.rotate(90, 0, 1, 0);
+			drawTableAndChairs(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelMatrix = popMatrix();
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(-5, 0, -15);
+			drawShelves(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelMatrix = popMatrix();
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(0, 1.5, -15);
+			drawTV(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelMatrix = popMatrix();
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(0, -2.5, 0)
+			drawGround(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelMatrix = popMatrix();
+		pushMatrix(modelMatrix);
+			modelMatrix.translate(0, 0, -20);
+			modelMatrix.rotate(90, 1, 0, 0);
+			drawGround(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelmatrix = popMatrix();
+		requestAnimationFrame(draw(gl, u_ModelMatrix, u_NormalMatrix));
+	}
 }
 
 function initArrayBuffer(gl, attribute, data, num, type){  // Assigns atttribute to the array buffer containing data with vSize n and type type
-    var buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-    var a_attribute = gl.getAttribLocation(gl.program, attribute);
-    gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-    gl.enableVertexAttribArray(a_attribute);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-    return true;
+	var buffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+	var a_attribute = gl.getAttribLocation(gl.program, attribute);
+	gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+	gl.enableVertexAttribArray(a_attribute);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+	return true;
 }
 
 var g_matrixStack = [];
 function pushMatrix(m){
-    var m2 = new Matrix4(m);
-    g_matrixStack.push(m2);
+	var m2 = new Matrix4(m);
+	g_matrixStack.push(m2);
 }
 
 function popMatrix(){
-    return g_matrixStack.pop();
+	return g_matrixStack.pop();
 }
--- src/models/shapes.js
+++ src/models/shapes.js
@@ -1,166 +1,166 @@
 function initCylinderVertexBuffers(gl, ratio, isLampShade){
-    var step = 6;
-    var vertices = [];
-    var colors = [];
-    var normals = [];
-    var indices = [];
-    for(var i = 0; i <= 720 + step; i += step){
-        vertices.push(ratio*Math.cos(i*Math.PI/180)); vertices.push(-0.5); vertices.push(ratio*Math.sin(i*Math.PI/180));
-        vertices.push(Math.cos(i*Math.PI/180)); vertices.push(0.5); vertices.push(Math.sin(i*Math.PI/180));
-        indices.push(i/step);
-        colors.push(1); colors.push(0); colors.push(0); colors.push((isLampShade) ? 0.5 : 1);
-        normals.push(Math.cos(i*Math.PI/180)); normals.push(0); normals.push(Math.sin(i*Math.PI/180));
-        normals.push(Math.cos(i*Math.PI/180)); normals.push(0); normals.push(Math.sin(i*Math.PI/180));
-    }
-    vertices = new Float32Array(vertices);
-    colors = new Float32Array(colors);
-    normals = new Float32Array(normals);
-    indices = new Uint8Array(indices);
-    if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
-    if (!initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT)) return -1;
-    if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
+	var step = 6;
+	var vertices = [];
+	var colors = [];
+	var normals = [];
+	var indices = [];
+	for(var i = 0; i <= 720 + step; i += step){
+		vertices.push(ratio*Math.cos(i*Math.PI/180)); vertices.push(-0.5); vertices.push(ratio*Math.sin(i*Math.PI/180));
+		vertices.push(Math.cos(i*Math.PI/180)); vertices.push(0.5); vertices.push(Math.sin(i*Math.PI/180));
+		indices.push(i/step);
+		colors.push(1); colors.push(0); colors.push(0); colors.push((isLampShade) ? 0.5 : 1);
+		normals.push(Math.cos(i*Math.PI/180)); normals.push(0); normals.push(Math.sin(i*Math.PI/180));
+		normals.push(Math.cos(i*Math.PI/180)); normals.push(0); normals.push(Math.sin(i*Math.PI/180));
+	}
+	vertices = new Float32Array(vertices);
+	colors = new Float32Array(colors);
+	normals = new Float32Array(normals);
+	indices = new Uint8Array(indices);
+	if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
+	if (!initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT)) return -1;
+	if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
   
-    var indexBuffer = gl.createBuffer();
-    if (!indexBuffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-    }
+	var indexBuffer = gl.createBuffer();
+	if (!indexBuffer) {
+	console.log('Failed to create the buffer object');
+	return false;
+	}
   
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
   
-    return indices.length;
+	return indices.length;
   }
 
 function drawCylinder(gl, u_ModelMatrix, u_NormalMatrix, n){
-    pushMatrix(modelMatrix);
-        gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-        g_normalMatrix.setInverseOf(modelMatrix);
-        g_normalMatrix.transpose();
-        gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
-        gl.drawElements(gl.TRIANGLE_STRIP, n, gl.UNSIGNED_BYTE, 0);
-    modelMatrix = popMatrix();
+	pushMatrix(modelMatrix);
+		gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+		g_normalMatrix.setInverseOf(modelMatrix);
+		g_normalMatrix.transpose();
+		gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
+		gl.drawElements(gl.TRIANGLE_STRIP, n, gl.UNSIGNED_BYTE, 0);
+	modelMatrix = popMatrix();
 }
 
 function initCircleVertexBuffers(gl){
-    var step = 6;
-    var vertices = [0, 0, 0];
-    var colors = [1, 0, 0, 1];
-    var normals = [0, 1, 0];
-    var indices = [0];
-    for(var i = 0; i <= 360; i += step){
-        vertices.push(Math.cos(i*Math.PI/180)); vertices.push(0); vertices.push(Math.sin(i*Math.PI/180));
-        colors.push(1); colors.push(0); colors.push(0); colors.push(1);
-        normals.push(0); normals.push(1); normals.push(0);
-        indices.push(i/step + 1);
-    }
-    vertices = new Float32Array(vertices);
-    colors = new Float32Array(colors);
-    normals = new Float32Array(normals);
-    indices = new Uint8Array(indices);
-    if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
-    if (!initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT)) return -1;
-    if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
+	var step = 6;
+	var vertices = [0, 0, 0];
+	var colors = [1, 0, 0, 1];
+	var normals = [0, 1, 0];
+	var indices = [0];
+	for(var i = 0; i <= 360; i += step){
+		vertices.push(Math.cos(i*Math.PI/180)); vertices.push(0); vertices.push(Math.sin(i*Math.PI/180));
+		colors.push(1); colors.push(0); colors.push(0); colors.push(1);
+		normals.push(0); normals.push(1); normals.push(0);
+		indices.push(i/step + 1);
+	}
+	vertices = new Float32Array(vertices);
+	colors = new Float32Array(colors);
+	normals = new Float32Array(normals);
+	indices = new Uint8Array(indices);
+	if (!initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT)) return -1;
+	if (!initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT)) return -1;
+	if (!initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT)) return -1;
   
-    var indexBuffer = gl.createBuffer();
-    if (!indexBuffer) {
-    console.log('Failed to create the buffer object');
-    return false;
-    }
+	var indexBuffer = gl.createBuffer();
+	if (!indexBuffer) {
+	console.log('Failed to create the buffer object');
+	return false;
+	}
   
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
   
-    return indices.length;
+	return indices.length;
 }
 
 function drawCircle(gl, u_ModelMatrix, u_NormalMatrix, n) {
-    pushMatrix(modelMatrix);
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-    g_normalMatrix.setInverseOf(modelMatrix);
-    g_normalMatrix.transpose();
-    gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
-    gl.drawElements(gl.TRIANGLE_FAN, n, gl.UNSIGNED_BYTE, 0);
-    modelMatrix = popMatrix();
+	pushMatrix(modelMatrix);
+	gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+	g_normalMatrix.setInverseOf(modelMatrix);
+	g_normalMatrix.transpose();
+	gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
+	gl.drawElements(gl.TRIANGLE_FAN, n, gl.UNSIGNED_BYTE, 0);
+	modelMatrix = popMatrix();
 }
 
 function drawSolidCylinder(gl, u_ModelMatrix, u_NormalMatrix){
-    n = initCircleVertexBuffers(gl);
-    pushMatrix(modelMatrix);
-        modelMatrix.translate(0, 0.5, 0);
-        drawCircle(gl, u_ModelMatrix, u_NormalMatrix, n);
-        modelMatrix.translate(0, -1, 0);
-        modelMatrix.rotate(180, 1, 0, 0);
-        drawCircle(gl, u_ModelMatrix, u_NormalMatrix, n);
-    modelMatrix = popMatrix();
-    n = initCylinderVertexBuffers(gl, 1);
-    drawCylinder(gl, u_ModelMatrix, u_NormalMatrix, n);
+	n = initCircleVertexBuffers(gl);
+	pushMatrix(modelMatrix);
+		modelMatrix.translate(0, 0.5, 0);
+		drawCircle(gl, u_ModelMatrix, u_NormalMatrix, n);
+		modelMatrix.translate(0, -1, 0);
+		modelMatrix.rotate(180, 1, 0, 0);
+		drawCircle(gl, u_ModelMatrix, u_NormalMatrix, n);
+	modelMatrix = popMatrix();
+	n = initCylinderVertexBuffers(gl, 1);
+	drawCylinder(gl, u_ModelMatrix, u_NormalMatrix, n);
 }
 
 function initVertexBuffers(gl){
-    // Create a cube
-    //    v6----- v5
-    //   /|      /|
-    //  v1------v0|
-    //  | |     | |
-    //  | |v7---|-|v4
-    //  |/      |/
-    //  v2------v3
-    var vertices = new Float32Array([   // Coordinates
-        0.5, 0.5, 0.5,  -0.5, 0.5, 0.5,  -0.5,-0.5, 0.5,   0.5,-0.5, 0.5, // v0-v1-v2-v3 front
-        0.5, 0.5, 0.5,   0.5,-0.5, 0.5,   0.5,-0.5,-0.5,   0.5, 0.5,-0.5, // v0-v3-v4-v5 right
-        0.5, 0.5, 0.5,   0.5, 0.5,-0.5,  -0.5, 0.5,-0.5,  -0.5, 0.5, 0.5, // v0-v5-v6-v1 up
-        -0.5, 0.5, 0.5,  -0.5, 0.5,-0.5,  -0.5,-0.5,-0.5,  -0.5,-0.5, 0.5, // v1-v6-v7-v2 left
-        -0.5,-0.5,-0.5,   0.5,-0.5,-0.5,   0.5,-0.5, 0.5,  -0.5,-0.5, 0.5, // v7-v4-v3-v2 down
-        0.5,-0.5,-0.5,  -0.5,-0.5,-0.5,  -0.5, 0.5,-0.5,   0.5, 0.5,-0.5  // v4-v7-v6-v5 back
-    ]);
-    var colors = new Float32Array([    // Colors
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,     // v0-v1-v2-v3 front
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,     // v0-v3-v4-v5 right
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,     // v0-v5-v6-v1 up
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,     // v1-v6-v7-v2 left
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,     // v7-v4-v3-v2 down
-        1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1　    // v4-v7-v6-v5 back
-    ]);
-    var normals = new Float32Array([    // Normal
-        0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
-        1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
-        0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
-        -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
-        0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
-        0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
-    ]);
-    // Indices of the vertices
-    var indices = new Uint8Array([
-        0, 1, 2,   0, 2, 3,    // front
-        4, 5, 6,   4, 6, 7,    // right
-        8, 9,10,   8,10,11,    // up
-        12,13,14,  12,14,15,    // left
-        16,17,18,  16,18,19,    // down
-        20,21,22,  20,22,23     // back
-    ]);
-    initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT);
-    initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT);
-    initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT);
-    var indexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-    return indices.length;
+	// Create a cube
+	//	v6----- v5
+	//   /|	  /|
+	//  v1------v0|
+	//  | |	 | |
+	//  | |v7---|-|v4
+	//  |/	  |/
+	//  v2------v3
+	var vertices = new Float32Array([   // Coordinates
+		0.5, 0.5, 0.5,  -0.5, 0.5, 0.5,  -0.5,-0.5, 0.5,   0.5,-0.5, 0.5, // v0-v1-v2-v3 front
+		0.5, 0.5, 0.5,   0.5,-0.5, 0.5,   0.5,-0.5,-0.5,   0.5, 0.5,-0.5, // v0-v3-v4-v5 right
+		0.5, 0.5, 0.5,   0.5, 0.5,-0.5,  -0.5, 0.5,-0.5,  -0.5, 0.5, 0.5, // v0-v5-v6-v1 up
+		-0.5, 0.5, 0.5,  -0.5, 0.5,-0.5,  -0.5,-0.5,-0.5,  -0.5,-0.5, 0.5, // v1-v6-v7-v2 left
+		-0.5,-0.5,-0.5,   0.5,-0.5,-0.5,   0.5,-0.5, 0.5,  -0.5,-0.5, 0.5, // v7-v4-v3-v2 down
+		0.5,-0.5,-0.5,  -0.5,-0.5,-0.5,  -0.5, 0.5,-0.5,   0.5, 0.5,-0.5  // v4-v7-v6-v5 back
+	]);
+	var colors = new Float32Array([	// Colors
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,	 // v0-v1-v2-v3 front
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,	 // v0-v3-v4-v5 right
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,	 // v0-v5-v6-v1 up
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,	 // v1-v6-v7-v2 left
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1,	 // v7-v4-v3-v2 down
+		1, 0, 0, 1,   1, 0, 0, 1,   1, 0, 0, 1,  1, 0, 0, 1　	// v4-v7-v6-v5 back
+	]);
+	var normals = new Float32Array([	// Normal
+		0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+		1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+		0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+		-1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+		0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+		0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+	]);
+	// Indices of the vertices
+	var indices = new Uint8Array([
+		0, 1, 2,   0, 2, 3,	// front
+		4, 5, 6,   4, 6, 7,	// right
+		8, 9,10,   8,10,11,	// up
+		12,13,14,  12,14,15,	// left
+		16,17,18,  16,18,19,	// down
+		20,21,22,  20,22,23	 // back
+	]);
+	initArrayBuffer(gl, 'a_Position', vertices, 3, gl.FLOAT);
+	initArrayBuffer(gl, 'a_Color', colors, 4, gl.FLOAT);
+	initArrayBuffer(gl, 'a_Normal', normals, 3, gl.FLOAT);
+	var indexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+	return indices.length;
 }
 
 function drawbox(gl, u_ModelMatrix, u_NormalMatrix, n) {
-    pushMatrix(modelMatrix);
+	pushMatrix(modelMatrix);
 
-        // Pass the model matrix to the uniform variable
-        gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+		// Pass the model matrix to the uniform variable
+		gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
 
-        // Calculate the normal transformation matrix and pass it to u_NormalMatrix
-        g_normalMatrix.setInverseOf(modelMatrix);
-        g_normalMatrix.transpose();
-        gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
+		// Calculate the normal transformation matrix and pass it to u_NormalMatrix
+		g_normalMatrix.setInverseOf(modelMatrix);
+		g_normalMatrix.transpose();
+		gl.uniformMatrix4fv(u_NormalMatrix, false, g_normalMatrix.elements);
 
-        // Draw the cube
-        gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+		// Draw the cube
+		gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 
-    modelMatrix = popMatrix();
+	modelMatrix = popMatrix();
 }
